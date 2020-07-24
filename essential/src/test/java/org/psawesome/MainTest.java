59aafac0f471f8ded80c23309db4f5130c8d1cb9
ps <wiv821253@gmail.com>
2020-07-25 05:27:27 +0900

    add test case

--- essential/src/test/java/org/psawesome/MainTest.java
+++ essential/src/test/java/org/psawesome/MainTest.java
@@ -1,9 +1,12 @@
 package org.psawesome;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
 
 /**
  * @author ps [https://github.com/wiv33/b-spring5-reactive]
@@ -16,15 +19,56 @@ import reactor.core.publisher.Flux;
  * @see
  * @since 20. 7. 25. Saturday
  */
+@DisplayName("Flux Mono 출력으로 확인하는 테스트 코드 by subscribe ")
 public class MainTest {
   private final static Logger log = LoggerFactory.getLogger(MainTest.class);
+
   @Test
-  void testFluxRange() {
+  @DisplayName("")
+  void testFluxReduceAndScan() {
     Flux.range(1, 5)
             .reduce(0, Integer::sum)
-            .subscribe(
-                    res -> log.info("result : {}", res)
-            );
+            .subscribe(res -> log.info("result : {}", res));
+/*
+05:07:06.846 [Test worker] INFO org.psawesome.MainTest - result : 15
+ */
+
+    log.info(" ===================== split ===================== ");
+
+    Flux.range(1, 5)
+            .scan(0, Integer::sum)
+            .subscribe(res -> log.info("result : {}", res));
+    /*
+05:07:06.850 [Test worker] INFO org.psawesome.MainTest - result : 0
+05:07:06.851 [Test worker] INFO org.psawesome.MainTest - result : 1
+05:07:06.851 [Test worker] INFO org.psawesome.MainTest - result : 3
+05:07:06.851 [Test worker] INFO org.psawesome.MainTest - result : 6
+05:07:06.851 [Test worker] INFO org.psawesome.MainTest - result : 10
+05:07:06.851 [Test worker] INFO org.psawesome.MainTest - result : 15
+     */
   }
 
+  @Test
+  @DisplayName("scan을 이용하여 스트림의 평균 이동을 계산할 수 있다.")
+  void testFluxScan() {
+    int bucketSize = 5; // 다섯 가지의 이벤트 == 관심있는 이벤트
+    Flux.range(1, 500)
+            .index() // 각 원소에 인덱스를 부여한다.
+            .log()
+//            .filter(n -> n.getT1() % 3 == 0)
+            .scan(
+                    new int[bucketSize],
+                    (acc, elem) -> {
+                      acc[elem.getT1().intValue() % bucketSize] = elem.getT2();
+                      return acc;
+                    })
+            .log()
+    .skip(bucketSize)
+            .log()
+    .map(arr -> Arrays.stream(arr).sum() * 1.0 / bucketSize)
+    .subscribe(average -> log.info("Running average : {}", average),
+            throwable -> log.error(throwable.getMessage()),
+            () -> log.info("onComplete"),
+            subscription -> subscription.request(Long.MAX_VALUE));
+  }
 }
