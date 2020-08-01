b539f0203f447a39d3a99c92f8037ba033ca4fc2
ps <wiv821253@gmail.com>
2020-08-01 14:02:32 +0900

    move module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,5 @@ rootProject.name = "b-spring5-reactive"
 include("temperature")
 include("essential")
 include("functionalApplication")
-include("practice-netty-server")
-include("practice-netty-second", "practice-netty-third", "practice-netty-last")
+include("practice-netty:practice-netty-server","practice-netty:practice-netty-second", "practice-netty:practice-netty-third", "practice-netty:practice-netty-last")
 
