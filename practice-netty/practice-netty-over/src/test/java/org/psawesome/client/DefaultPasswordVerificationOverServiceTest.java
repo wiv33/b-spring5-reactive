8b87a5869719d2a585cea0e3dc187d59ac32536b
ps <wiv821253@gmail.com>
2020-08-02 15:52:43 +0900

    add practice last over netty server

--- practice-netty/practice-netty-over/src/test/java/org/psawesome/client/DefaultPasswordVerificationOverServiceTest.java
+++ practice-netty/practice-netty-over/src/test/java/org/psawesome/client/DefaultPasswordVerificationOverServiceTest.java
@@ -1,6 +1,18 @@
 package org.psawesome.client;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.psawesome.common.PsPasswordDTO;
+import org.psawesome.server.PasswordVerificationApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 /**
  * @author ps [https://github.com/wiv33/b-spring5-reactive]
@@ -15,6 +27,37 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class DefaultPasswordVerificationOverServiceTest {
 
+  PasswordEncoder encoder;
 
+  WebClient client;
 
+  @BeforeEach
+  void setUp() {
+    new Thread(PasswordVerificationApplication::main).start();
+    encoder = new BCryptPasswordEncoder(18);
+    client = WebClient.builder()
+            .baseUrl("http://localhost:8080")
+            .build();
+  }
+
+  @Test
+  void testPasswordVerificationFromServiceByProjectReactor() {
+    StepVerifier.create(client
+            .post()
+            .uri("/check")
+            .body(BodyInserters.fromPublisher(
+                    Mono.just(new PsPasswordDTO("psawesome", encoder.encode("psawesome")))
+                    , PsPasswordDTO.class))
+            .exchange()
+    )
+            .expectSubscription()
+            .thenAwait(Duration.ofSeconds(10))
+            .assertNext(
+                    response -> Assertions.assertTrue(response.statusCode().is2xxSuccessful())
+            )
+            .expectComplete()
+            .verifyThenAssertThat()
+            .tookLessThan(Duration.ofSeconds(20));
+
+  }
 }
